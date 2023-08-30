fb6a08ac16ae20df859c353e485559b737e1b0a4
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-08-30 10:05:36 +0330

    create pages folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,9 @@
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Main from "./components/Main/Main";
-import Sidebarinlockmode from "@/app/components/Sidebarinlockmode/Sidebarinlockmode";
-import Link from "next/link";
+import Home from "@/Pages/Home/Home";
 
 export default function Home() {
   return (
-    <div className="container flexp-col ">
-      <div className="">
-        <Header />
-      </div>
-      <div className="flex relative bottom-4 ">
-        <div className="w-9/12 mr-1 ">
-          <Main />
-        </div>
-        <div className="w-3/12 ml-1 ">
-          <Sidebar /> <Sidebarinlockmode />
-        </div>
-      </div>
-      <Footer />
+    <div className="">
+      <Home />
     </div>
   );
 }
