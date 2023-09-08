6567cbcc763e899c6903d68620fdaead8fc403ef
Mojtaba Ahmadpour <ahmadpour1997@gmail.com>
2023-09-09 02:07:01 +0330

    create login and signin folder

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+
+const login = () => {
+    return (  
+        <main>
+            <div classname=''>
+                <h1>login</h1>
+            </div>
+        </main>
+    );
+}
+ 
+export default login;
---
+++ src/app/signin/page.jsx
@@ -0,0 +1,11 @@
+const signin = () => {
+    return (
+        <main>
+            <div classname=''>
+                <h1>sign in </h1>
+            </div>
+        </main>
+     );
+}
+ 
+export default signin;
